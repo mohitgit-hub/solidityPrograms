253dd7bb49fafc964f4604111ec82296383b17e9
Mohit Dad(Maheshwari) <123006118+mohitgit-hub@users.noreply.github.com>
2024-10-25 11:04:11 +0530

    Update wallet.sol

--- wallet.sol
+++ wallet.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-
-// CA- 0xd9145CCE52D386f254917e481eB44e9943F39138
+
 contract SimpleWallet {
     address owner;
     string public str; 
